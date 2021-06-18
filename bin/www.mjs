3473eaf1358653efa228ddad62fbea97c1f1e77d
Alexis Hevia <hevia.alexis@gmail.com>
2021-06-17 22:35:21 -0500

    read AUTH_USERNAME instead of API_USERNAME env variable

--- bin/www.mjs
+++ bin/www.mjs
@@ -6,8 +6,8 @@ const {
   PORT,
   PERSISTENCE_TYPE,
   PERSISTENCE_FILEPATH,
-  API_USERNAME,
-  API_PASSWORD,
+  AUTH_USERNAME,
+  AUTH_PASSWORD,
 } = process.env;
 
 const port = PORT || 8080;
@@ -17,8 +17,8 @@ const persistence = {
 };
 const authentication = {
   type: "basic",
-  username: API_USERNAME,
-  password: API_PASSWORD,
+  username: AUTH_USERNAME,
+  password: AUTH_PASSWORD,
 };
 
 async function main() {
